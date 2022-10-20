7c21853112d314e037db797e57556c24a6b0b7eb
QuQyouth <2745914927@qq.com>
2022-10-20 10:44:47 +0800

    修改默认路由

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -3,21 +3,19 @@ import { WelcomeFirst } from "../components/welcome/WelcomeFirst";
 import { WelcomeForth } from "../components/welcome/WelcomeForth";
 import { WelcomeSecond } from "../components/welcome/WelcomeSecond";
 import { WelcomeThird } from "../components/welcome/WelcomeThird";
-import { Bar } from "../views/Bar";
-import { Foo } from "../views/Foo";
 import { Welcome } from "../views/Welcome";
 
 export const routes: RouteRecordRaw[] = [
-    {path:'/', component: Foo},
-    {path:'/about', component:Bar},
+    {path:'/', redirect: '/welcome'},
     {
         path:'/welcome',
         component: Welcome,
         children:[
-            {path: '1',component:WelcomeFirst},
-            {path: '2',component:WelcomeSecond},
-            {path: '3',component:WelcomeThird},
-            {path: '4',component:WelcomeForth}
+            {path: '', redirect:'/welcome/1'},
+            {path: '1', component:WelcomeFirst},
+            {path: '2', component:WelcomeSecond},
+            {path: '3', component:WelcomeThird},
+            {path: '4', component:WelcomeForth}
         ]
     }
 ]
