bcc6c1fe406d7036f59973782bd093244f5e2970
drunkzhuo <2745914927@qq.com>
2022-10-26 12:44:23 +0800

    wip: 支出收入tab切换

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,16 +1,22 @@
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
 import { Icon } from "../../shared/Icon";
+import { Tab, Tabs } from "../../shared/Tabs";
 import s from './ItemCreate.module.scss';
 export const ItemCreate = defineComponent({
     setup: (props,context) => {
+        const refKind = ref('支出')
         return () => (
             <MainLayout>
-                 {{
+                {{
                     title: () => '记一笔',
                     icon: () => <Icon name="doubleLeft"/>,
                     default: () => <>
-                        <div>main</div>
+                        {/* <Tabs selected = {refKind.value} onUpdateSelected={name => refKind.value = name}> */}
+                        <Tabs v-model:selected={refKind.value}>
+                            <Tab name="支出"></Tab>
+                            <Tab name="收入"></Tab>
+                        </Tabs>
                     </>
                 }}
             </MainLayout>
