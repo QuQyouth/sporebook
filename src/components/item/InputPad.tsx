66b150786f3fe9414b88ccd18c60ef814a6f97e2
drunkzhuo <2745914927@qq.com>
2022-10-26 21:22:51 +0800

    尝试使用grid布局

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,9 +1,17 @@
 import dayjs from "dayjs";
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { Icon } from "../../shared/Icon";
 import s from './InputPad.module.scss';
+import { DatetimePicker, Popup } from 'vant';
+
 export const InputPad = defineComponent({
+    // components: {
+    //     [DatetimePicker.name]: DatetimePicker,
+    // },
     setup: (props,context) => {
+        const refShowPop = ref(false)
+        const refCurrentDate = ref<Date>(new Date())
+        const refAmount = ref('')
         const buttons = [
             {text: '1', onClick: ()=>{}},
             {text: '2', onClick: ()=>{}},
@@ -19,19 +27,34 @@ export const InputPad = defineComponent({
             {text: '-', onClick: ()=>{}},
             {text: '.', onClick: ()=>{}},
             {text: '0', onClick: ()=>{}},
-            {text: '清空', onClick: ()=>{}},
+            {text: '清空', onClick: ()=>{refAmount.value = ''}},
             {text: '完成', onClick: ()=>{}},
         ]
+        const appendText = (n: number | string) => refAmount.value += n.toString()
+        const hideDatePicker = () => refShowPop.value = false
+        const showDatePicker = () => refShowPop.value = true
+        const setDate = (date: Date) => {refCurrentDate.value = date; hideDatePicker()}
         return () => <>
             <div>
                 <div class={s.padNav}>
-                    <div class={s.amount}>
-                        88888
-                    </div>
-                    <div class={s.date}>
+                    <span class={s.amount}>
+                        {refAmount}
+                    </span>
+                    <span class={s.date}>
                         <Icon name='note' />
-                        <span>{dayjs().format('YYYY-MM-DD HH:mm:ss')}</span>
-                    </div>
+                        <span onClick={showDatePicker}>
+                            {dayjs(refCurrentDate.value).format('YYYY-MM-DD HH:mm:ss')}
+                        </span>
+                        <Popup position="bottom" v-model:show={refShowPop.value}>
+                            <DatetimePicker
+                                value={refCurrentDate.value}
+                                type="date"
+                                title="选择年月日"
+                                onConfirm={setDate}
+                                onCancel={hideDatePicker}
+                            />
+                        </Popup>
+                    </span>
                 </div>
                 <div class={s.buttons}>
                     {buttons.map(item=>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 // @ts-nocheck
 import { svgstore } from './src/vite_plugins/svgstore';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
+import styleImport from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +16,17 @@ export default defineConfig({
       mergeProps: true
     }),
     svgstore(),
+    Components({
+      resolvers: [VantResolver()],
+    }),
+    styleImport({
+      libs: [
+        {
+          libraryName: 'vant',
+          esModule: true,
+          resolveStyle: (name) => `vant/es/${name}/style/index`
+        },
+      ],
+    }),
   ]
 })
