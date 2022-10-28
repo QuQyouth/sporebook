c513d57769a021b0591a6f7698a8b14cc280a558
drunkzhuo <2745914927@qq.com>
2022-10-28 17:57:58 +0800

    准备实现减法

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -3,7 +3,6 @@ import { ComputedRef, defineComponent, reactive, ref } from "vue";
 import { Icon } from "../../shared/Icon";
 import s from './InputPad.module.scss';
 import { DatetimePicker, Popup } from 'vant';
-import { number } from "echarts";
 
 export const InputPad = defineComponent({
     // components: {
@@ -12,8 +11,11 @@ export const InputPad = defineComponent({
     setup: (props,context) => {
         const refShowPop = ref(false)
         const refCurrentDate = ref<Date>(new Date())
-        const refAmount = ref('')
+        const refAmount = ref('0') //当前未输入完的数据
         // const refAmountList = reactive<number[]>([]) //<number[]>添加泛型
+        const refNumberList = reactive({pre:'0',cur:'0'})
+        const refDisplayAmount = ref('') //这里refDisplayAmount + refAmount是需要展示的数据
+        const refSign = ref('')
         const buttons = [
             {text: '1', onClick: ()=>{appendText(1)}},
             {text: '2', onClick: ()=>{appendText(2)}},
@@ -22,16 +24,22 @@ export const InputPad = defineComponent({
             {text: '4', onClick: ()=>{appendText(4)}},
             {text: '5', onClick: ()=>{appendText(5)}},
             {text: '6', onClick: ()=>{appendText(6)}},
-            {text: '+', onClick: ()=>{appendText('+')}},
+            {text: '+', onClick: ()=>{addOrSubtract("+")}},
             {text: '7', onClick: ()=>{appendText(7)}},
             {text: '8', onClick: ()=>{appendText(8)}},
             {text: '9', onClick: ()=>{appendText(9)}},
-            {text: '-', onClick: ()=>{appendText('-')}},
+            {text: '-', onClick: ()=>{addOrSubtract('-')}},
             {text: '.', onClick: ()=>{appendText('.')}},
             {text: '0', onClick: ()=>{appendText(0)}},
-            {text: '清空', onClick: ()=>{refAmount.value = '0'}},
+            {text: '清空', onClick: ()=>{operationEmpty()}},
             {text: '=', onClick: ()=>{performOperation('=')}},
         ]
+        const operationEmpty = () => {
+            refAmount.value = '0'
+            refNumberList.pre = '0'
+            refNumberList.cur = '0'
+            refDisplayAmount.value = ''
+        }
         const performOperation = (e: "=") => {
             const result = eval(refAmount.value)
             refAmount.value = result.toFixed(2)
@@ -39,6 +47,25 @@ export const InputPad = defineComponent({
         const deleteLast = () => {
             refAmount.value = refAmount.value.substring(0, refAmount.value.length - 1)
         }
+
+        const addOrSubtract = (e: '+'|'-') => {
+            // 这里注意toFixed返回的是string; Number可以去掉小数点后多余的0
+            refNumberList.cur = parseFloat(refAmount.value).toFixed(2)
+            const preNumber = parseFloat(refNumberList.pre)
+            const curNumber = parseFloat(refNumberList.cur)
+            if(e === '+'){
+                refNumberList.pre = Number((curNumber + preNumber).toFixed(2)).toString()
+            }else{
+                debugger
+                refNumberList.pre = Number((curNumber - preNumber).toFixed(2)).toString()
+            }
+            debugger
+            refDisplayAmount.value = refNumberList.pre + e
+            refAmount.value = ''
+            refNumberList.cur = '0'
+        }
+
+        //每个数字的逻辑
         const appendText = (n: number | string) => {
             const nString = n.toString()
             const dotIndex = refAmount.value.indexOf('.')
@@ -46,24 +73,17 @@ export const InputPad = defineComponent({
                 return
             }
 
-            if (dotIndex >= 0 && refAmount.value.length - dotIndex > 2) return
+            // if (dotIndex >= 0 && refAmount.value.length - dotIndex > 2) return
 
             if (nString === '.') {
-                const currentList = [...refAmount.value]
-                const signList = currentList.filter(item => item === "+" || item === "-" )
                 
-                const numberList = refAmount.value.split(/[+]|-/)
-
-                // for (const item in numberList) {
-                //     // 数字列表中的每一项已经存在小数点
-                    
-                //     if (item.indexOf('.') >= 0) {
-                //         return
-                //     }
-                // }
+                // const currentList = [...refAmount.value]
+                // const signList = currentList.filter(item => item === "+" || item === "-" )
+                
+                // const numberList = refAmount.value.split(/[+]|-/)
                 
                 if (dotIndex >= 0) { // 已经有小数点了
-                return
+                    return
                 }
             } else if (nString === '0') {
                 if (dotIndex === -1) { // 没有小数点
@@ -85,7 +105,7 @@ export const InputPad = defineComponent({
             <div>
                 <div class={s.padNav}>
                     <span class={s.amount}>
-                        {refAmount.value}
+                        {refDisplayAmount.value + refAmount.value}
                     </span>
                     <span class={s.date}>
                         <Icon name='note' />
