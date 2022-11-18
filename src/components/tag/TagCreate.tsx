89358d32bb2cbff1147a6e5b580fb4b8d159c8bb
drunkzhuo <2745914927@qq.com>
2022-11-18 14:20:32 +0800

    准备实现validate

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,54 +1,66 @@
-import { defineComponent } from "vue";
-import { MainLayout } from "../../layouts/MainLayout";
-import { Icon } from "../../shared/Icon";
+import { defineComponent, PropType, reactive, toRaw } from 'vue';
+import { MainLayout } from '../../layouts/MainLayout';
+import { Button } from '../../shared/Button';
+import { EmojiSelect } from '../../shared/EmojiSelect';
+import { Icon } from '../../shared/Icon';
 import s from './TagCreate.module.scss';
 export const TagCreate = defineComponent({
-    setup: (props,context) => {
-        return () => (
-            <MainLayout>
-                {{
-                    title: () => '新建标签',
-                    icon: () => <Icon name="doubleLeft" onClick={()=>{}}/>,
-                    default: () => (
-                        <form action="">
-                            <div>
-                                <label>
-                                    <span>name</span>
-                                    <input type="text" />
-                                </label>
-                            </div>
-                            
-                            <div>
-                                <label>
-                                    <span>sign</span>
-                                    <div>
-                                        <nav>
-                                            <span>表情</span>
-                                            <span>表情</span>
-                                            <span>表情</span>
-                                            <span>表情</span>
-                                            <span>表情</span>
-                                            <span>表情</span>
-                                        </nav>
-                                        <ol>
-                                            <li>1</li>
-                                            <li>1</li>
-                                            <li>1</li>
-                                        </ol>
-                                    </div>
-                                </label>
-                            </div>
-                            <div>
-                                <p>长按编辑</p>
-                            </div>
-                            <div>
-                                <button>确定</button>
-                            </div>
-                            
-                        </form>
-                    )
-                }}
-            </MainLayout>
-        )
+  props: {
+    name: {
+      type: String as PropType<string>
+    },
+  },
+  setup: (props, context) => {
+    const formData = reactive({
+      name: '',
+      sign: 'x',
+    })
+    const onSubmit = (e: Event)=>{
+      console.log(toRaw(formData)); //拿到原始值
+      const rules = [
+        {key: 'name', required: true, message: '必填'},
+        {key: 'name', pattern: /^.{1,4}$/, message: '输入1~4个字符'}
+      ]
+      // const errors = validate(formData, rules)
+      e.preventDefault()
     }
+    return () => (
+      <MainLayout>{{
+        title: () => '新建标签',
+        icon: () => <Icon name="doubleLeft" onClick={() => { }} />,
+        default: () => (
+          <form class={s.form} onSubmit={onSubmit}>
+            <div class={s.formRow}>
+              <label class={s.formLabel}>
+                <span class={s.formItem_name}>标签名</span>
+                <div class={s.formItem_value}>
+                  <input v-model={formData.name} class={[s.formItem, s.input, s.error]}></input>
+                </div>
+                <div class={s.formItem_errorHint}>
+                  {/* <span>{errors['name'][0]}</span> */}
+                </div>
+              </label>
+            </div>
+            <div class={s.formRow}>
+              <label class={s.formLabel}>
+                <span class={s.formItem_name}>符号 {formData.sign}</span>
+                <div class={s.formItem_value}>
+                  <EmojiSelect v-model={formData.sign} class={[s.formItem, s.emojiList, s.error]} />
+                </div>
+                <div class={s.formItem_errorHint}>
+                  <span>必填</span>
+                </div>
+              </label>
+            </div>
+            <p class={s.tips}>记账时长按标签即可进行编辑</p>
+            <div class={s.formRow}>
+              <div class={s.formItem_value}>
+                <Button class={[s.formItem, s.button]}>确定</Button>
+              </div>
+            </div>
+          </form>
+        )
+      }}</MainLayout>
+    )
+  }
 })
