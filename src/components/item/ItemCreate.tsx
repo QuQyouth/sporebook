f7c19ff22fa9f3d6386ab71b70f17385a1b92ddb
drunkzhuo <2745914927@qq.com>
2022-11-22 13:14:23 +0800

    解决tag和键盘重叠问题

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -7,6 +7,24 @@ import s from './ItemCreate.module.scss';
 export const ItemCreate = defineComponent({
     setup: (props,context) => {
         const refKind = ref('支出')
+        const refExpensesTags = ref([
+            {id: 1, name: '吃', sign: '￥', category: 'expenses'},
+            {id: 1, name: '吃', sign: '￥', category: 'expenses'},
+            {id: 1, name: '吃', sign: '￥', category: 'expenses'},
+            {id: 1, name: '吃', sign: '￥', category: 'expenses'},
+            {id: 1, name: '吃', sign: '￥', category: 'expenses'},
+            {id: 1, name: '吃', sign: '￥', category: 'expenses'},
+            {id: 1, name: '吃', sign: '￥', category: 'expenses'},
+            {id: 1, name: '吃', sign: '￥', category: 'expenses'},
+            {id: 1, name: '吃', sign: '￥', category: 'expenses'},
+            {id: 1, name: '吃', sign: '￥', category: 'expenses'},
+            {id: 1, name: '吃', sign: '￥', category: 'expenses'},
+            {id: 1, name: '吃', sign: '￥', category: 'expenses'},
+            {id: 1, name: '吃', sign: '￥', category: 'expenses'},
+        ])
+        const refIncomeTags = ref([
+            {id: 2, name: '工资', sign: '￥', category: 'expenses'}
+        ])
         return () => (
             <MainLayout>
                 {{
@@ -15,8 +33,30 @@ export const ItemCreate = defineComponent({
                     default: () => <>
                         {/* <Tabs selected = {refKind.value} onUpdateSelected={name => refKind.value = name}> */}
                         <Tabs v-model:selected={refKind.value}>
-                            <Tab name="支出">tab1</Tab>
-                            <Tab name="收入">tab2</Tab>
+                            <Tab name="支出" class={s.tags_wrapper}>
+                                {refExpensesTags.value.map((tag) => {
+                                    return <div class={s.tag}>
+                                        <div class={s.sign}>
+                                            {tag.sign}
+                                        </div>
+                                        <div class={s.name}>
+                                            {tag.name}
+                                        </div>
+                                    </div>
+                                })}
+                            </Tab>
+                            <Tab name="收入" class={s.tags_wrapper}>
+                            {refIncomeTags.value.map((tag) => {
+                                    return <div class={s.tag}>
+                                    <div class={s.sign}>
+                                        {tag.sign}
+                                    </div>
+                                    <div class={s.name}>
+                                        {tag.name}
+                                    </div>
+                                </div>
+                                })}
+                            </Tab>
                         </Tabs>
                         <div class={s.inputPad_wrapper}>
                             <InputPad />
