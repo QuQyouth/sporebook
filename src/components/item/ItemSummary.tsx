265ec8cc61fb58ba5025be2443f05ec7e1576a91
drunkzhuo <2745914927@qq.com>
2022-11-23 11:22:59 +0800

    实现ItemSummary布局

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -13,8 +13,24 @@ export const ItemSummary = defineComponent({
     },
     setup: (props,context) => {
         return () => (
-            <div>
-                {props.startDate}
+            <div class={s.wrapper}>
+                <ul class={s.total}>
+                    <li><span>收入</span><span>100</span></li>
+                    <li><span>支出</span><span>100</span></li>
+                    <li><span>净收入</span><span>100</span></li>
+                </ul>
+                <ol class={s.list}>
+                    <li>
+                        <div class={s.sign}>sign</div>
+                        <div class={s.tag}>吃</div>
+                        <div class={s.amount}>10</div>
+                    </li>
+                    <li>
+                        <div class={s.sign}>sign</div>
+                        <div class={s.tag}>吃</div>
+                        <div class={s.amount}>10</div>
+                    </li>
+                </ol>
             </div>
         )
     }
