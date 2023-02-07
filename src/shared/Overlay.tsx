c5071fe3f9a89c21b4fd8c23a8d8a1b24ea67d95
drunkzhuo <2745914927@qq.com>
2023-02-07 20:23:20 +0800

    添加退出登录

--- src/shared/Overlay.tsx
+++ src/shared/Overlay.tsx
@@ -1,6 +1,8 @@
-import { defineComponent, PropType, ref } from "vue";
+import { Dialog } from "vant";
+import { defineComponent, onMounted, PropType, reactive, ref } from "vue";
 import { RouterLink } from "vue-router";
 import { Icon } from "../shared/Icon";
+import { useMeStore } from "../stores/useMeStore";
 import s from './Overlay.module.scss';
 
 export const Overlay = defineComponent({
@@ -10,18 +12,44 @@ export const Overlay = defineComponent({
         }
     },
     setup: (props,context) => {
+        const currentStore = useMeStore()
+        const {email} = currentStore
         const closeOverlay = () => {
             props.onClose?.()
         }
+        const refCurrentUser = ref<User>()
+        const onSignOut = async () => {
+            await Dialog.confirm({
+              title: '确认',
+              message: '你真的要退出登录吗？',
+            }).then(async ()=>{
+                await currentStore.logout()
+                
+                localStorage.removeItem('jwt')
+                window.location.reload()
+            })
+
+        }
+        onMounted(async () => {
+            // const response = await currentStore.
+        })
         return () => (
             <>
                 <div class={s.mask} onClick={closeOverlay}>
                     <div class={s.overlay}>
                         <section class={s.currentUser}>
-                            <RouterLink to={`/sign_in`}>
+                            {email ? (
+                                <div>
+                                    <h2 class={s.email}>{email}</h2>
+                                    <p onClick={onSignOut}>点击这里退出登录</p>
+                                </div>
+                            ):(
+                                <RouterLink to={`/sign_in`}>
                                 <h2>未登录用户</h2>
                                 <p>点击这里登录</p>
                             </RouterLink>
+                            )}
+                            
                         </section>
                         <nav class={s.action}>
                             <ul class={s.action_list}>
